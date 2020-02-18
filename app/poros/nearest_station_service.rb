19ae1be25f60d121034799ad0b9577e22add4fae
Tyla Devon <tyladevon@gmail.com>
2020-02-17 21:01:59 -0700

    Add find three method in Service Poro

--- app/poros/nearest_station_service.rb
+++ app/poros/nearest_station_service.rb
@@ -12,7 +12,16 @@ class NearestStationService
     end 
     JSON.parse(raw_results.body, symbolize_names: true) 
   end 
-  
+
+  def find_three
+    raw_results = conn.get("/api/alt-fuel-stations/v1/nearest.json") do |req|
+      req.params['fuel_type'] = 'ELEC'
+      req.params['location'] = @location
+      req.params['limit'] = 3
+      req.params['api_key'] = ENV['NREL_API_KEY']
+    end 
+    JSON.parse(raw_results.body, symbolize_names: true)
+  end 
   private 
   
   def conn 
