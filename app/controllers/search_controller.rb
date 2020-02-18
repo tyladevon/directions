a4791135c1c993614b17b7e1191c64651b71036e
Tyla Devon <tyladevon@gmail.com>
2020-02-17 21:01:32 -0700

    Refactor Search Controller index method. Add top three stations local.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,8 @@ class SearchController < ApplicationController
   def index 
     render locals: {
       station: SearchFacade.new(params[:location]).find_nearest_station,
-      directions: SearchFacade.new(params[:location]).find_directions
+      directions: SearchFacade.new(params[:location]).find_directions,
+      top_stations: SearchFacade.new(params[:location]).top_three
     }
-    # render locals: {
-    #   station: NearestStationSearch.new(params[:location]).search,
-    #   directions: DirectionsSearch.new(params[:location]).search 
-    # }
   end
 end 
